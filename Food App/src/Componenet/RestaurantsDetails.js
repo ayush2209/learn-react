4dfae1f6ad09816e7fd06891bbde40d29b1a9f0d
ayush2209 <53752682+ayush2209@users.noreply.github.com>
2023-04-28 17:14:35 +0530

    WIP: Food App

--- Food App/src/Componenet/RestaurantsDetails.js
+++ Food App/src/Componenet/RestaurantsDetails.js
@@ -17,20 +17,17 @@ import Button from "react-bootstrap/Button";
 const Ratingcard = ({eachObj}) => {
   return (
     <Card className='res_details_rating'>
-      <div
-        className={eachObj?.avgRating >= 4 ? "avg_icon_good" : "avg_icon_bad"}
-      >
-        <div className='item-center'>
+      <span className={eachObj?.avgRating >= 4 ? "avg_icon_good" : "avg_icon_bad"} >
+        <span className='item-center'>
           <div>
             <i className='fa fa-solid fa-star'></i>
           </div>
-          <div className='ms-3'> {eachObj?.avgRating}</div>
-        </div>
-        <hr style={{margin: "5px"}} />
-        <div className='item-center restaurantRatings_totalRatings'>
+          <div className='ms-2'>{eachObj?.avgRating}</div>
+        </span>
+      </span>
+      <div className='item-center restaurantRatings_totalRatings'>
           {eachObj?.totalRatingsString}
         </div>
-      </div>
     </Card>
   );
 };
@@ -51,234 +48,160 @@ const RestaurantDetails = () => {
     const restaurant_Menu =
       jsonData?.data?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards;
     setRestaurantDetails(restaurant_Details);
+    console.log(restaurantDetails);
     setRestaurantMenu(restaurant_Menu);
   }
 
   return (
-    <div>
-      <Container>
-        <Row className='mt-3 mb-3'>
-          <Col>
-            <BreadcrumbComponent />
-          </Col>
-        </Row>
-        <Row className=''>
-          <Col className=''>
-            <div className='restaurantNameStyle'>{restaurantDetails?.name}</div>
-            <p className='restaurant-Name-Add'>
-              {restaurantDetails?.cuisines?.join(" , ")}{" "}
-            </p>
-            <div className='restaurant-Name-Add'>
-              {restaurantDetails?.areaName}
-            </div>
-          </Col>
-          <Col className='' style={{justifyContent: "end", display: "flex"}}>
-            <Ratingcard eachObj={restaurantDetails} />
-          </Col>
-        </Row>
-        <Row className='mt-3'>
-          <Col>
-            {" "}
-            <p>{restaurantDetails?.feeDetails?.message}</p>{" "}
-          </Col>
-        </Row>
-        <hr />
-        <div className='inline-item RestaurantTimeCost'>
-          <p> 26 Minutes</p>
-          <p style={{marginLeft: "5px"}}>
-            {"   |  "}
-            {restaurantDetails?.costForTwoMessage}
-          </p>
-        </div>
-        <div className='inline-item'>
-          <div>
-            <BootstrapCard content={"Card 1"} />
-          </div>
-          <div>
-            {" "}
-            <BootstrapCard content={"Card 2"} />
-          </div>
-          <div>
-            {" "}
-            <BootstrapCard content={"Card 3"} />
-          </div>
-          <div>
-            {" "}
-            <BootstrapCard content={"Card 4"} />
-          </div>
+    <Container>
+      <Row className='mt-3 mb-3'>
+        <Col>
+          <BreadcrumbComponent />
+        </Col>
+      </Row>
+      <Row className=''>
+        <Col className=''>
+          <div className='restaurantNameStyle'>{restaurantDetails?.name} </div>
+          <p className='restaurant-Name-Add'> {restaurantDetails?.cuisines?.join(" , ")} </p>
+          <div className='restaurant-Name-Add'>{restaurantDetails?.areaName} </div>
+        </Col>
+        <Col className='' style={{justifyContent: "end", display: "flex"}}>
+          <Ratingcard eachObj={restaurantDetails} />
+        </Col>
+      </Row>
+      <Row className='mt-3'>
+        <Col>
+          <p className='restaurant-Name-Add'> {restaurantDetails?.feeDetails?.message} </p>
+        </Col>
+      </Row>
+      <hr className='new' />
+      <div className='inline-item RestaurantTimeCost'>
+        <p> 26 Minutes</p>
+        {"  | "}
+        <p style={{marginLeft: "5px"}}> {restaurantDetails?.costForTwoMessage} </p>
+      </div>
+      <div className='inline-item'>
+        <div>
+          <BootstrapCard content={"Card 1"} />
         </div>
         <div>
-          <Form className='inline-item vegOnly'>
+          <BootstrapCard content={"Card 2"} />
+        </div>
+        <div>
+          <BootstrapCard content={"Card 3"} />
+        </div>
+        <div>
+          <BootstrapCard content={"Card 4"} />
+        </div>
+      </div>
+      <div>
+        {restaurantDetails?.veg ? (
+          <div className='pureVeg' style={{gap:'5px'}}>
+            <span>
+              <i class="fa fa-sharp fa-light fa-leaf" style={{color:'#65931f'}}></i>
+            </span>
+            <span>Pure Veg</span>
+          </div>
+        ) : (
+          <Form className='inline-item vegOnly' style={{gap:'0px'}}>
             <div>Veg Only</div>{" "}
             <div className='ms-3'>
-              {" "}
               <Form.Check type='switch' id='custom-switch' />
             </div>
           </Form>
-        </div>
-        <hr />
-        <div className='menu-items-list'>
-          {restaurantMenu?.map((item, index) =>
-            item?.card?.card?.title?.length > 0 ? (
-              <>
-                <Accordion defaultActiveKey={["0"]} alwaysOpen>
-                  <Accordion.Item eventKey='0' key={index}>
-                    <Accordion.Header>
-                      <div className='recommended'>
-                        {item?.card?.card?.title}{" "}
-                        {item?.card?.card?.itemCards
-                          ? item?.card?.card?.itemCards?.length
-                          : ""}
-                      </div>
-                    </Accordion.Header>
-                    <Accordion.Body>
-                      {item?.card?.card?.itemCards
-                        ? item?.card?.card?.itemCards?.map((eachItem) => (
-                            <BootstrapCard
-                              key={eachItem?.card?.info?.id}
-                              content={
+        )}
+      </div>
+      <hr className='new' />
+      <div className='menu-items-list'>
+        {restaurantMenu?.map((item, index) =>
+          item?.card?.card?.title?.length > 0 ? (
+            <>
+              <Accordion defaultActiveKey={["0"]} alwaysOpen>
+                <Accordion.Item eventKey='0' key={index}>
+                  <Accordion.Header>
+                    <div className='recommended'>
+                      {item?.card?.card?.title}{" "}
+                      {item?.card?.card?.itemCards ? item?.card?.card?.itemCards?.length : ""}
+                    </div>
+                  </Accordion.Header>
+                  <Accordion.Body>
+                    {item?.card?.card?.itemCards ? item?.card?.card?.itemCards?.map((eachItem) => (
+                      <BootstrapCard key={eachItem?.card?.info?.id} content={
+                        <div className='menu_Reataurant_Align'>
+                            <div>
+                              <div> {eachItem?.card?.info?.isVeg ?
+                                  ( <div className='vej_Item'>Vej</div> ) :
+                                  ( <div className='nonVej_Item'>Nonvej</div> )
+                              }
+                              </div>
+                              <h3 className='mt-2 styles_itemNameText'>  {eachItem?.card?.info?.name} </h3>
+                              <div className="price-Font"> {eachItem?.card?.info?.price > 0 ?
+                                new Intl.NumberFormat("en-IN", {style: "currency",currency: "INR", }).format( eachItem?.card?.info?.price / 100 ) : " "}
+                              </div>
+                            </div>
+                            <div className='menu-img-wrapper'>
+                              { <img className='menu-Img' src={ RES_IMG_URL + eachItem?.card?.info?.imageId } alt={eachItem?.card?.info?.name} /> }
+                                <div className="item-center">
+                                  <button className="btn btn-sm btn-outline-primary"> Add to cart </button>
+                                </div>
+                            </div>
+                        </div>
+                      } />
+                      )) : item?.card?.card?.categories?.map((eachItem, index) => (
+                        <div key={index}>
+                          <div className='submenu'> {eachItem.title} ( {eachItem?.itemCards?.length} ) </div>
+                            <div className='categoriesListCard'> {eachItem?.itemCards?.map((catogaryMenuList) => (
+                              <BootstrapCard key={catogaryMenuList?.card?.info?.id} content= {
                                 <div className='menu_Reataurant_Align'>
                                   <div>
-                                    <div>
-                                      {eachItem?.card?.info?.isVeg ? (
-                                        <div className='vej_Item'>Vej</div>
-                                      ) : (
-                                        <div className='nonVej_Item'>
-                                          Nonvej
-                                        </div>
-                                      )}
-                                    </div>
-                                    <h3 className='mt-2 styles_itemNameText'>
-                                      {" "}
-                                      {eachItem?.card?.info?.name}
-                                    </h3>
-                                    <div>
-                                      {eachItem?.card?.info?.price > 0
-                                        ? new Intl.NumberFormat("en-IN", {
-                                            style: "currency",
-                                            currency: "INR",
-                                          }).format(
-                                            eachItem?.card?.info?.price / 100
-                                          )
-                                        : " "}
-                                    </div>
-                                    <div className='menu-description'>
-                                      {eachItem?.card?.info?.description}
-                                    </div>
-                                  </div>
-                                  <div className='menu-img-wrapper'>
-                                    {
-                                      <img
-                                        className='menu-Img'
-                                        src={
-                                          RES_IMG_URL +
-                                          eachItem?.card?.info?.imageId
+                                    <div> {catogaryMenuList?.card?.info ?.isVeg ? (
+                                      <div className='vej_Item'> Vej </div>
+                                        ) : (
+                                          <div className='nonVej_Item'> Nonvej </div>
+                                        )}
+                                      </div>
+                                      <h3 className='mt-2 styles_itemNameText'> {catogaryMenuList?.card?.info?.name} </h3>
+                                      <div> {catogaryMenuList?.card?.info?.price > 0 ? new Intl.NumberFormat("en-IN", {style: "currency", currency: "INR",
+                                              }).format(catogaryMenuList?.card?.info?.price / 100) : " "}
+                                      </div>
+                                      </div>
+                                      <div className='menu-img-wrapper'>
+                                        {
+                                          <img
+                                            className='menu-Img'
+                                            src={
+                                              RES_IMG_URL +
+                                              catogaryMenuList?.card?.info
+                                                ?.imageId
+                                            }
+                                            alt={
+                                              catogaryMenuList?.card?.info?.name
+                                            }
+                                          />
                                         }
-                                        alt={eachItem?.card?.info?.name}
-                                      />
-                                    }
-                                    <button className='add-btn'> ADD +</button>
-                                  </div>
-                                </div>
-                              }
-                            />
-                          ))
-                        : item?.card?.card?.categories?.map(
-                            (eachItem, index) =>
-                              console.log("ayush", eachItem) || (
-                                <div key={index}>
-                                  <div className='submenu'>
-                                    {eachItem.title} ({" "}
-                                    {eachItem?.itemCards?.length} )
-                                  </div>
-                                  <div className='categoriesListCard'>
-                                    {eachItem?.itemCards?.map(
-                                      (catogaryMenuList) => (
-                                        <BootstrapCard
-                                          key={catogaryMenuList?.card?.info?.id}
-                                          content={
-                                            <div className='menu_Reataurant_Align'>
-                                              <div>
-                                                <div>
-                                                  {catogaryMenuList?.card?.info
-                                                    ?.isVeg ? (
-                                                    <div className='vej_Item'>
-                                                      {" "}
-                                                      Vej{" "}
-                                                    </div>
-                                                  ) : (
-                                                    <div className='nonVej_Item'>
-                                                      Nonvej
-                                                    </div>
-                                                  )}
-                                                </div>
-                                                <h3 className='mt-2 styles_itemNameText'>
-                                                  {" "}
-                                                  {
-                                                    catogaryMenuList?.card?.info
-                                                      ?.name
-                                                  }
-                                                </h3>
-                                                <div>
-                                                  {catogaryMenuList?.card?.info
-                                                    ?.price > 0
-                                                    ? new Intl.NumberFormat(
-                                                        "en-IN",
-                                                        {
-                                                          style: "currency",
-                                                          currency: "INR",
-                                                        }
-                                                      ).format(
-                                                        catogaryMenuList?.card
-                                                          ?.info?.price / 100
-                                                      )
-                                                    : " "}
-                                                </div>
-                                                <div className='menu-description'>
-                                                  { catogaryMenuList?.card?.info?.description }
-                                                </div>
-                                              </div>
-                                              <div className='menu-img-wrapper'>
-                                                {
-                                                  <img
-                                                    className='menu-Img'
-                                                    src={
-                                                      RES_IMG_URL +
-                                                      catogaryMenuList?.card
-                                                        ?.info?.imageId
-                                                    }
-                                                    alt={
-                                                      catogaryMenuList?.card
-                                                        ?.info?.name
-                                                    }
-                                                  />
-                                                }
-                                                <button className='add-btn'>
-                                                  {" "}
-                                                  ADD +
-                                                </button>
-                                              </div>
-                                            </div>
-                                          }
-                                        />
-                                      )
-                                    )}
-                                  </div>
-                                </div>
-                              )
-                          )}
-                    </Accordion.Body>
-                  </Accordion.Item>
-                </Accordion>
-                <hr />
-              </>
-            ) : (
-              " "
-            )
-          )}
-        </div>
-      </Container>
-    </div>
+                                        <button >
+                                          {" "}
+                                        Add to cart
+                                        </button>
+                                      </div>
+                                    </div>
+                                  }
+                                />
+                              ))}
+                            </div>
+                        </div>
+                        ))}
+                  </Accordion.Body>
+                </Accordion.Item>
+              </Accordion>
+              <hr />
+            </>
+          ) : (
+            " "
+          )
+        )}
+      </div>
+    </Container>
   );
 };
 export default RestaurantDetails;
